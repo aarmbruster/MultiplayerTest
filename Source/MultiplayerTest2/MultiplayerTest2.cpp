e6347039fc2fcca3a4b6a7872e248477389a35bd
aarmbruster <andrew.armbruster@gmail.com>
2016-11-19 13:22:08 -0600

    initial project adds

---
+++ Source/MultiplayerTest2/MultiplayerTest2.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MultiplayerTest2.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MultiplayerTest2, "MultiplayerTest2" );
---
+++ Source/MultiplayerTest2/MultiplayerTest2GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "MultiplayerTest2GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERTEST2_API AMultiplayerTest2GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
